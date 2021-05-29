c462078065eec5339ff52c4635bdb3c9ea7f5b43
nanawebdev <nanaprettysinger@gmail.com>
2021-05-29 15:01:48 +0300

    Фикс багов ESLint

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
 const getRandomInteger = (from, to) => {
-  if (from >= to) {
-    alert("Введите корректный диапазон: значение \"до\" должно быть больше значения \"от\"")
-    return
+  if (from >= to || from < 0) {
+    return;
   }
 
-  if (from < 0) {
-    alert("Введите значение больше 0 (включительно)")
-    return
-  }
-
-  return Math.floor(from + Math.random() * (to - from + 1))
-}
+  return Math.floor(from + Math.random() * (to - from + 1));
+};
 
 const getRandomFloatNumber = (from, to, precision) => {
-   if (from >= to) {
-    alert("Введите корректный диапазон: значение \"до\" должно быть больше значения \"от\"")
-     return
-  }
-  if (from < 0) {
-    alert("Введите значение больше 0 (включительно)")
-    return
+  if (from >= to || from < 0) {
+    return;
   }
 
-  let randomNumber = from + Math.random() * (to - from)
-  return parseFloat(randomNumber.toFixed(precision))
-}
+  const randomNumber = from + Math.random() * (to - from);
+  return parseFloat(randomNumber.toFixed(precision));
+};
 
-console.log(getRandomInteger(1.2, 2.4))
-console.log(getRandomFloatNumber(1.1, 1.2, 2))
+getRandomInteger(3, 7);
+getRandomFloatNumber(1.1, 1.2, 2);
